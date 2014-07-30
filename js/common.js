e517a04b393f4020a86cff69a3971a318583edba
akella <akella.a@gmail.com>
2014-07-30 17:31:01 +0300

    sticky + maps

--- js/common.js
+++ js/common.js
@@ -3,9 +3,11 @@ head.ready(function() {
 	// $(document).on("click", function(){
 	// 	$(".js-popup").hide();
 	// });
+	
+	$(".catalog4-2-left").sticky({topSpacing:10});
+
 	gotomenu($( ".pg__menu li" ).index( $('.pg__menu li.is-active')));
 	function gotomenu(n){
-		console.log('goto'+n);
 		$('.pg__menuline')
 			.css('left',$('.pg__menu li').eq(n).position().left)
 			.css('width',$('.pg__menu li').eq(n).children('a').width());
@@ -13,11 +15,8 @@ head.ready(function() {
 
 	$('.pg__menu li').hover(function() {
 		gotomenu($( ".pg__menu li" ).index( $(this) ));
-		console.log($('.pg__menu li').eq(2).position().left);
 	}, function() {
-		/* Stuff to do when the mouse leaves the element */
 		gotomenu($( ".pg__menu li" ).index( $('.pg__menu li.is-active')));
-
 	});
 
 });
--- js/init.js
+++ js/init.js
@@ -1,3 +1,4 @@
 head.load("js/lib/jquery.js", "js/lib/fotorama.js", "js/lib/popup.js",
 					"js/lib/cycle.js",
+					"js/lib/sticky.js",
 					"js/common.js");
